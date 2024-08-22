565b8cbe143f5d426564fd4e2dd137ff0c72811f
devaanshi15 <devaanshic1509@gmail.com>
2024-08-23 01:08:55 +0530

    3

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>TransformationsPage</div>
+  )
+}
+
+export default TransformationsPage
+
+
+//dynamic routes as it wont be same for all users.
+// localhost:3000/transformation/[id]
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddTransformationTypePage = () => {
+  return (
+    <div>AddTransformationTypePage</div>
+  )
+}
+
+export default AddTransformationTypePage
+
+//type dynamic route is allowing us to create diff types of transformations
