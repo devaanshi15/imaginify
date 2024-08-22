9fdd73ffa426aa57b7fb35d18d2c926ba2a3e1d4
devaanshi15 <devaanshic1509@gmail.com>
2024-08-23 00:33:14 +0530

    2

---
+++ app/(root)/credits/page.tsx
@@ -0,0 +1,11 @@
+//using for purchase the credits for ai image generation
+import React from 'react'
+
+const CreditPage = () => {
+    return (
+        <div>CreditPage</div>
+    )
+}
+
+export default CreditPage
+
