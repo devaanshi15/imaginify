8179db6b23521b9a01e679e1163c369456709abc
devaanshi15 <devaanshic1509@gmail.com>
2024-08-27 00:09:31 +0530

    almost

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -24,11 +24,10 @@ const Sidebar = () => {
                 const isActive = link.route === pathname
 
                 return (
-                  <li key={link.route} className={`sidebar-nav_element group ${
-                    isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
-                  }`}>
+                  <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
+                    }`}>
                     <Link className="sidebar-link" href={link.route}>
-                      <Image 
+                      <Image
                         src={link.icon}
                         alt="logo"
                         width={24}
@@ -40,7 +39,7 @@ const Sidebar = () => {
                   </li>
                 )
               })}
-              </ul>
+            </ul>
 
 
             <ul className="sidebar-nav_elements">
@@ -48,11 +47,10 @@ const Sidebar = () => {
                 const isActive = link.route === pathname
 
                 return (
-                  <li key={link.route} className={`sidebar-nav_element group ${
-                    isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
-                  }`}>
+                  <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
+                    }`}>
                     <Link className="sidebar-link" href={link.route}>
-                      <Image 
+                      <Image
                         src={link.icon}
                         alt="logo"
                         width={24}
